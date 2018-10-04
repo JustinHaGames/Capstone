8cd993ff22b0c1bef816e1cf020fd46611d65365
Justin Ha <justinha22@gmail.com>
2018-10-04 16:15:47 -0400

    Comic Player can now enter Reality, time to start on the surge minigame

--- Capstone/Assets/Scripts/Reality/RealityPlayerMovement.cs
+++ Capstone/Assets/Scripts/Reality/RealityPlayerMovement.cs
@@ -18,11 +18,19 @@ public class RealityPlayerMovement : MonoBehaviour {
 	public float boundaryL; 
 	public float boundaryR;
 
+	public GameObject comicPlayer; 
+
+	bool imagining; 
+
+	bool inactive; 
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		box = GetComponent<BoxCollider2D>();
 		sprite = GetComponent<SpriteRenderer> ();
+
+		inactive = false;
 
 	}
 
@@ -32,41 +40,49 @@ public class RealityPlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
-		//Press the left and right keys to move
-		bool right = Input.GetKey(KeyCode.RightArrow);
-		bool left = Input.GetKey (KeyCode.LeftArrow);
+		if (!inactive) {
+			//Press the left and right keys to move
+			bool right = Input.GetKey (KeyCode.RightArrow);
+			bool left = Input.GetKey (KeyCode.LeftArrow);
 
-		//If right or left if pressed, accel in that direction
-		if (right) {
-			vel.x += accel;
-			if (!left) {
-				sprite.flipX = false;
+			//If right or left if pressed, accel in that direction
+			if (right) {
+				vel.x += accel;
+				if (!left) {
+					sprite.flipX = false;
+				}
 			}
-		}
-		if (left) {
-			vel.x -= accel;
-			if (!right) {
-				sprite.flipX = true;
+			if (left) {
+				vel.x -= accel;
+				if (!right) {
+					sprite.flipX = true;
+				}
 			}
+
+			//Limit the player's max velocity
+			vel.x = Mathf.Max (Mathf.Min (vel.x, maxAccel), -maxAccel);
+
+			//If you don't move right or left, then don't move
+			if ((!right && !left) || (right && left)) {
+				vel.x = 0;
+			}
+
+			//If player is trying to go out of bounds
+			if (transform.position.x <= boundaryL) {
+				transform.position = new Vector3 (boundaryL, transform.position.y, transform.position.z); 
+			} else if (transform.position.x >= boundaryR) {
+				transform.position = new Vector3 (boundaryR, transform.position.y, transform.position.z); 
+				imagining = true; 
+				inactive = true; 
+			}
+
+			rb.MovePosition ((Vector2)transform.position + vel * Time.deltaTime);
 		}
 
-		//Limit the player's max velocity
-		vel.x = Mathf.Max (Mathf.Min (vel.x, maxAccel), -maxAccel);
-
-		//If you don't move right or left, then don't move
-		if ((!right && !left) || (right && left)) {
-			vel.x = 0;
+		if (imagining) {
+			Instantiate (comicPlayer, new Vector3 (transform.position.x + 8f, transform.position.y, transform.position.z), Quaternion.identity);
+			imagining = false; 
 		}
-
-		//If player is trying to go out of bounds
-		if (transform.position.x <= boundaryL) {
-			transform.position = new Vector3 (boundaryL, transform.position.y, transform.position.z); 
-		} else if (transform.position.x >= boundaryR) {
-			transform.position = new Vector3 (boundaryR, transform.position.y, transform.position.z); 
-		}
-			
-
-		rb.MovePosition ((Vector2)transform.position + vel * Time.deltaTime);
 
 	}
 
