283970383401172b440db4996ec4f4b281f48fb9
Justin Ha <justinha22@gmail.com>
2018-10-30 16:25:14 -0400

    Added box gravity physics

---
+++ Capstone/Assets/Scripts/MechanicTest/Box.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour {
+
+	Vector2 vel; 
+
+	Rigidbody2D rb; 
+	BoxCollider2D box; 
+
+	bool grounded; 
+
+	public float gravity; 
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		box = GetComponent<BoxCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Grounded (); 
+
+		Debug.Log (grounded);
+
+		rb.MovePosition ((Vector2)transform.position + vel * Time.deltaTime);
+
+	}
+
+	void Grounded(){
+		Vector2 pt1 = transform.TransformPoint (box.offset + new Vector2 (box.size.x / 2, -box.size.y / 2));
+		Vector2 pt2 = transform.TransformPoint (box.offset - (box.size / 2) + new Vector2 (0, 0));
+
+		grounded = Physics2D.OverlapArea(pt1, pt2, LayerMask.GetMask("Platform")) != null; 
+
+		if (grounded) {
+		} else {
+			vel.y += gravity;
+		}
+
+	}
+}
