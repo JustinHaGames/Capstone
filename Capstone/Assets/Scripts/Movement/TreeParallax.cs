78a305a99346bff39eb5844584f72d4bc76de00e
Justin Ha <justinha22@gmail.com>
2018-12-13 01:10:51 -0500

    1st dream scene done

---
+++ Capstone/Assets/Scripts/Movement/TreeParallax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeParallax : MonoBehaviour {
+
+	public float parallaxSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!GameManager.instance.monarchComeAlive) {
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				transform.Translate (Vector2.left * parallaxSpeed * Time.deltaTime);
+			} else if (Input.GetKey (KeyCode.LeftArrow)) {
+				transform.Translate (Vector2.right * parallaxSpeed * Time.deltaTime);
+			}
+		}
+	}
+}
